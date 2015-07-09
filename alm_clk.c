2b906e3d5e645c478b2bdb9a738f5442d7a3b2ad
John Easton <jte13c@my.fsu.edu>
2015-07-09 10:30:57 -0400

    working on reading inputs

--- alm_clk.c
+++ alm_clk.c
@@ -1,6 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
-//#include "uart/uart.h"
+#include <avr/cpufunc.h>
 #include "twi/i2cmaster.h"
 
 #define F_CPU 8000000UL
@@ -26,20 +26,32 @@
 #define DIG2 (1<<PB2)
 #define DIG1 (1<<PB1)
 
+//switch matrix (rows and columns)
+#define C0 (1<<PC0)
+#define C1 (1<<PC1)
+#define C2 (1<<PC2)
+#define C3 (1<<PC3)
+#define R0 (1<<PB4)
+#define R1 (1<<PB5)
+#define R2 (1<<PB6)
+#define R3 (1<<PB7)
+
 void dummy_read(void);
 void draw(unsigned char, unsigned char);
 void rtc_write(uint8_t*);
 void alarm(uint8_t);
+void input_test(uint16_t);
+uint16_t input_read();
 
 int main(void){
 
-	uint8_t rtc_data[7] = {0x00, //0 seconds
-						0x00,    //1: minutes
-						0x00,    //2: hours
-						0x01,    //3: day of week
-						0x01,    //4: day of month
-						0x01,    //5: month
-						0x15,    //6: year
+	uint8_t rtc_data[7] = {0x00, //0: seconds
+			       0x00,    //1: minutes
+			       0x00,    //2: hours
+			       0x01,    //3: day of week
+			       0x01,    //4: day of month
+			       0x01,    //5: month
+			       0x15,    //6: year
 	};
 	unsigned char digit[4] = {9, 8, 7, 6};
 	uint16_t inputs; //each switch is a bit
@@ -52,6 +64,9 @@ int main(void){
 		DDRD |= (A_SEG + B_SEG + C_SEG + D_SEG + E_SEG + F_SEG + G_SEG + DIG4);
 		DDRB |= (DIG1 + DIG2 + DIG3);
 
+		PORTD |= (A_SEG + B_SEG+ C_SEG + D_SEG + E_SEG + F_SEG + G_SEG + DIG4);
+		PORTB |= (DIG1 + DIG2 + DIG3);
+		
 		i2c_start_wait(DS1307+I2C_WRITE);     // set device address and write mode
    		i2c_write(0x00);                      // set register address
    		i2c_rep_start(DS1307+I2C_READ);       // set device address and read mode
@@ -61,6 +76,10 @@ int main(void){
 		i2c_stop();
 		dummy_read(); //Every other read returns invalid data. Hence, this.
 
+		inputs = input_read();
+		input_test(inputs);
+		
+		/*
 		for(uint8_t i = 4; i !=0; i--){
 			digit[0] = (rtc_data[1] & 0xF0) >> 4; //10 hours^W minutes
 			digit[1] = rtc_data[1] & 0x0F; //1 hours
@@ -69,15 +88,13 @@ int main(void){
 
 			_delay_ms(1); // do not remove
 			draw(digit[i-1], i);
-		}
+			}*/
 	}
 	return 0;
 }
 
 void draw(unsigned char digit, unsigned char disp){
 
-	PORTD |= (A_SEG + B_SEG+ C_SEG + D_SEG + E_SEG + F_SEG + G_SEG + DIG4);
-	PORTB |= (DIG1 + DIG2 + DIG3);
 
 	// turn off selected digit on the display (active high)
 	switch (disp){
@@ -147,14 +164,17 @@ void draw(unsigned char digit, unsigned char disp){
 		default:
 			break;
 	}
+	//turn it off
+	PORTD |= (A_SEG + B_SEG+ C_SEG + D_SEG + E_SEG + F_SEG + G_SEG + DIG4);
+	PORTB |= (DIG1 + DIG2 + DIG3);
 }
 
 void dummy_read(){
-  	 	i2c_start_wait(DS1307+I2C_WRITE);     // set device address and write mode
-   		i2c_write(0x00);                      // set register address
-   		i2c_rep_start(DS1307+I2C_READ);       // set device address and read mode
-		i2c_readAck();                    // read one byte from RTC
-		i2c_stop();
+  	i2c_start_wait(DS1307+I2C_WRITE);     // set device address and write mode
+	i2c_write(0x00);                      // set register address
+  	i2c_rep_start(DS1307+I2C_READ);       // set device address and read mode
+	i2c_readAck();                    // read one byte from RTC
+	i2c_stop();
 }
 
 void rtc_write(uint8_t *data){
@@ -166,14 +186,33 @@ void rtc_write(uint8_t *data){
     i2c_stop(); 
 }
 
-void input_test(){
+void input_test(uint16_t inputs){
+    for(int i=0; i<4; i++){
+	uint8_t tmp = (inputs<<(i*4)) & 0x0F;
+	draw(tmp, i+1);
+	_delay_ms(1);
+    }
+}
 
+uint16_t input_read(){
+    uint16_t inputs = 0xFFFF;
+    uint8_t rows[] = {R0, R1, R2, R3};
+    DDRC &= ~(C0 + C1 + C2 + C3); //set as inputs
+    PORTC &= ~(C0 + C1 + C2 + C3); //tristate
+    _NOP();
+    for(int i=0; i<4; i++){
+	DDRB |= rows[i];
+	inputs |= PINC & (C0 + C1 + C2 + C3);
+	_NOP();
+	inputs = inputs<<4;
+    }
+    return inputs;
 }
 
 void alarm(uint8_t on){
     if (on){
-	PORTB |= BUZZER; // turn on alarm
+	PORTB |= BUZZER; // turn on buzzer
     }else{
-	PORTB &= ~BUZZER;
+	PORTB &= ~BUZZER; // turn off buzzer
     }
 }
